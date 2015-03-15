de4da45cc7088b42f95bd1c6baf4ff4cccffd170
Bernardino Guerrero Ibarra <bernardino.guerrero@itesm.mx>
2015-03-14 23:29:18 -0600

    make relationship between Questions and choices
    make relationship between Questions and choices

---
+++ database/migrations/2015_03_15_051659_add_choices_questions_relationship.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddChoicesQuestionsRelationship extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::table('choices',function(Blueprint $table){
+			$table->integer('question_id')->unsigned()->nullable()->after('id');
+			$table->foreign('question_id')->references('id')->on('questions');
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('choices',function(Blueprint $table){
+			$table->dropForeign('choices_question_id_foreign');
+			$table->dropColumn('question_id');
+		});
+	}
+
+}
